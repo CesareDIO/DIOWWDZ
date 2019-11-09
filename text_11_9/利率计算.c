b446f05cf9cb1bc993d102c89d022283731506ca
CesareDIO <3420543991@qq.com>
2019-11-09 23:52:51 +0800

    初学

---
+++ text_11_9/利率计算.c
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+
+int main()
+{
+int money;
+printf("请输入金额");
+scanf("%d",&money);
+
+int year;
+printf("请输入年份");
+scanf("%d",&year);
+
+int rate;
+printf("请输入速率");
+scanf("%d",&rate);
+
+int interest=（money*1+money*rate)*year*yaear-money;
+scanf("输出利率%d",&interest);
+printf("输出利率%d \n",interest);
+ 
+return 0;
+
+}

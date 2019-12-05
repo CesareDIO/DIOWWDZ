61784b8f4b872aa747c8e1c097bd6c391f8cb65a
CesareDIO <3420543991@qq.com>
2019-12-05 21:31:30 +0800

    初学（存在细小问题）

---
+++ text_12_5/四则运算.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int A;
+    int B;
+    printf("«Î ‰»Î£∫%d %d\n",A,B); 
+    printf("%d+%d=%d\n",A,B,A+B);
+    printf("%d-%d=%d\n",A,B,A-B);
+    printf("%d*%d=%d\n",A,B,A*B);
+    printf("%d/%d=%d\n",A,B,A/B);
+    return 0;
+}
